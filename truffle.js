ee7a9d40580168c35d6c0f0d3a509fa8d70d1ec5
Byung Kwon <byungkwon96@gmail.com>
2022-02-24 17:17:56 +0900

    truffle setup

--- truffle.js
+++ truffle.js
@@ -1,1 +1,17 @@
 // truffle.js config for klaytn.
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = 'https://api.baobab.klaytn.net:8651'
+const PRIVATE_KEY = '0x0f3c1f0a8210dbf91749084db287f62084499475d14b7d94330f9eea2def6b42'
+
+module.exports ={
+    networks: {
+        klaytn: {
+            provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+            network_id: NETWORK_ID,
+            gas: GASLIMIT,
+            gasPrice: null,
+        }
+    },
+}
